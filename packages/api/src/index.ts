baf2b0305284375ee5e595bf939f0ecb8e2e36b6
Valentin Degenne <vdegenne@gmail.com>
2026-08-17 19:41:46 +0200

    {   lac-api: 0.1.2 }

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,42 @@
-export const PORT = 37283
+import {Endpoint, Rest} from '@vdegenne/mini-rest'
+
+export const DEFAULT_PORT = 37283
+
+export class Lac {
+	#rest: Rest<{
+		get: {'/': Endpoint<void, string>}
+		post: {'/': Endpoint<string, any>}
+	}>
+
+	constructor({
+		host = 'localhost',
+		port = DEFAULT_PORT,
+	}: {
+		/**
+		 * the IP address of the local machine running the LAC server (`lac-run`).
+		 */
+		host?: string
+
+		/**
+		 * Overrides the default port (37283)
+		 */
+		port?: number
+	} = {}) {
+		this.#rest = new Rest(`http://${host}:${port}`)
+	}
+
+	async get() {
+		const response = await this.#rest.get('/')
+		if (!response.ok) {
+			throw new Error('Make sure the server is running')
+		}
+		return response.text()
+	}
+
+	async set(value: string) {
+		const response = await this.#rest.postBody('/', value)
+		if (!response.ok) {
+			throw new Error('Make sure the server is running')
+		}
+	}
+}
