51159966ea11da09c1c4d507616a1b575e346c6b
vdegenne <vdegenne@gmail.com>
2026-08-20 12:50:44 +0000

    Build: (c946ccb) fix isComposing for mobile

--- assets/page-404-BeVeb_TA.js
+++
@@ -1,1 +0,0 @@
-import{b as p,t as g,g as i,w as m,i as v,s as w}from"./index-BYOF-q-e.js";import{P as O}from"./PageElement-CNKqIFpu.js";var P=Object.getOwnPropertyDescriptor,f=(t,s,n,a)=>{for(var e=a>1?void 0:a?P(s,n):s,r=t.length-1,o;r>=0;r--)(o=t[r])&&(e=o(e)||e);return e};let l=class extends O{render(){return p`404 NOT FOUND`}};l=f([g("page-404"),i(w),m(v``)],l);export{l as Page404};
